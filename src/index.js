7aab3e6183f41a4e50a50a347e53377bab70247c
Collin <collin22h@gmail.com>
2024-10-26 18:45:58 -0500

    refresh tokens

--- src/index.js
+++ src/index.js
@@ -47,4 +47,47 @@ app.post('/api/auth/token', async (c) => {
 	}
 });
 
+app.post('/api/auth/refresh', async (c) => {
+	const clientId = c.env.CLIENT_ID;
+	const clientSecret = c.env.CLIENT_SECRET;
+
+	const body = await c.req.json();
+	const now = Math.floor(Date.now() / 1000);
+
+	try {
+		const response = await fetch('https://oauth2.googleapis.com/token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				refresh_token: body.refresh_token,
+				client_id: clientId,
+				client_secret: clientSecret,
+				grant_type: 'refresh_token',
+			}),
+		});
+
+		if (response.ok) {
+			const { access_token: accessToken, expires_in: expiresIn } = await response.json();
+
+			return c.json(
+				{
+					accessToken,
+					expiresAt: now + expiresIn,
+				},
+				200
+			);
+		}
+
+		// handle for invalid refresh token
+		const data = await response.json();
+		console.error('request failed: ', data);
+		return c.json(data, 400);
+	} catch (error) {
+		console.error('Error with refresh_token request: ', error);
+		return c.json({ error }, 400);
+	}
+});
+
 export default app;
