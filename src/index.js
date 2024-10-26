38d5034bcb00046771dccb9e13695f9862ffca77
Collin <collin22h@gmail.com>
2024-10-26 18:23:18 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono();
+
+app.post('/api/auth/token', async (c) => {
+	const clientId = c.env.CLIENT_ID;
+	const clientSecret = c.env.CLIENT_SECRET;
+	const redirectUri = c.env.REDIRECT_URI;
+
+	const body = await c.req.json();
+	const now = Math.floor(Date.now() / 1000);
+
+	try {
+		const response = await fetch('https://oauth2.googleapis.com/token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				code: body.code,
+				client_id: clientId,
+				client_secret: clientSecret,
+				redirect_uri: redirectUri,
+				grant_type: 'authorization_code',
+			}),
+		});
+
+		if (response.ok) {
+			const { access_token: accessToken, expires_in: expiresIn, refresh_token: refreshToken } = await response.json();
+
+			return c.json(
+				{
+					accessToken,
+					expiresAt: now + expiresIn,
+					refreshToken,
+				},
+				200
+			);
+		}
+
+		const data = await response.json();
+		console.error('request failed: ', data);
+		return c.json(data, 400);
+	} catch (error) {
+		console.error('Error with authorization_code request: ', error);
+		return c.json({ error }, 400);
+	}
+});
+
+export default app;
